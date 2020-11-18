64956031627351a53cd0830dee57628d0510ec1e
buruopeng <72387022+buruopeng@users.noreply.github.com>
2020-11-18 12:53:35 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,14 +1,9 @@
 package 实验4;
-
-import java.util.Date;
 import java.io.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
-import java.io.IOException;
-import java.text.*;
-import java.util.ArrayList;
 public class Test {
     public static String txt2String(File file) {
         StringBuilder result = new StringBuilder();
@@ -24,7 +19,6 @@ public class Test {
         }
         return result.toString();
     }
-
     public static boolean writetxtfile(String Content, String filepath) {
         boolean flag = false;
         try {
@@ -39,7 +33,6 @@ public class Test {
         }
         return flag;
     }
-
     public static void main(String args[]) {
         File file = new File("D:\\java实验4\\B.txt");
         student abc = new student();
@@ -57,7 +50,6 @@ public class Test {
         Scanner class_name = new Scanner(System.in);
         abc.setClass_name(class_name.next());
         System.out.println(abc);
-        ;
         String s = "";
         char[] chars = txt2String(file).toCharArray();
         for (int i = 2; i < 240; i++) {
@@ -68,8 +60,6 @@ public class Test {
                 s += ",";
             }
         }
-
-
         try{b:while(true){System.out.println("选择功能：1.查询字或词出现的次数。2.退出程序");
          Scanner zi = new Scanner(System.in);
          int q=zi.nextInt();
@@ -81,37 +71,19 @@ public class Test {
                 String g = j.next();
                 int count = 0;
                 int index = 0;
-               ac:
-
-
-
-
                     while ((index = s.indexOf(g, index)) != -1) {
-
-
                         index = index + g.length();
                         count++;
-
                     }
-
-
                     System.out.println(g + "出现的次数：" + count);
-                    count = 0;
-
                 break;
             case 2:break b;
-
             default:
                 System.out.println("请输入1或2选择功能");
-
         }}}
         catch(Exception w){
             System.out.println("请输入数字选择功能！！！");
         }
-
-
-
-
         writetxtfile(abc+s, "D:\\java实验4\\A.txt");
 
     }
